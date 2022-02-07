70fce7eb8ddbb0f28e8f1b6713e9627c039d0d69
VSF <47865897+HashiY@users.noreply.github.com>
2022-02-07 17:02:59 -0300

    Parte 4

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ function flyShip(event) {
     }
 }
 
-//função de subir
+//funcao de subir
 function moveUp(){
     let topPosition = getComputedStyle(yourShip).getPropertyValue('top');//do css
     if(topPosition === "0px"){
@@ -28,7 +28,7 @@ function moveUp(){
     }
 }
 
-//função de descer
+//funcao de descer
 function moveDown() {
     let topPosition = getComputedStyle(yourShip).getPropertyValue('top');
     if(topPosition === "550px"){
@@ -38,6 +38,32 @@ function moveDown() {
         position += 50;                         //descer
         yourShip.style.top = `${position}px`;
     }
+}
+
+//funcionalidade de tiro
+function fireLaser() {
+    let laser = createLaserElement();
+    playArea.appendChild(laser);
+    moveLaser(laser);
+}
+
+//img de tiro
+function createLaserElement(){
+    //posicao do jogador
+    let xPosition = parseInt(window.getComputedStyle(yourShip).getPropertyValue('left'));
+    let yPosition = parseInt(window.getComputedStyle(yourShip).getPropertyValue('top'));
+    //criando a imagem
+    let newLaser = document.createElement('img');
+    newLaser.src = 'img/shoot.png';
+    newLaser.classList.add('laser'); // para editar no css
+    newLaser.style.left = `${xPosition}px`;
+    newLaser.style.top = `${yPosition - 10}px`; // para sair do meio da nave
+    return newLaser;
+}
+
+//movimentar o tiro
+function moveLaser(){
+
 }
 
 window.addEventListener("keydown", flyShip);
